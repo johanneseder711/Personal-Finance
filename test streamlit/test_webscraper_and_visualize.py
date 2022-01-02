a9718270b0a0fdfcbb50a43fd573c884b1fab161
Johannes Eder <johanneseder711@MacBook-Pro-von-Johannes.local>
2022-01-02 16:43:10 +0100

    first mvp implementation - flatex and raiffaisen working

--- test streamlit/test_webscraper_and_visualize.py
+++ test streamlit/test_webscraper_and_visualize.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -13,7 +7,21 @@ import time
 import streamlit as st
 
 
-# In[2]:
+# Functions
+
+def wait_for_full_load(driver, xpath):
+    '''
+    A function that takes in an xpath of an element and a driver (browser).
+    The function will be exited as soon as the element is available on the site.
+    '''
+    waiting = True
+    while waiting:
+        # check if at least one element is already available on the site
+        elements = driver.find_elements(By.XPATH, xpath)
+        if len(elements)!=0: 
+            return;
+        else:
+            time.sleep(1)
 
 
 # define the path to the credentials file
@@ -29,69 +37,124 @@ with open(PATH_CREDENTIALS) as f:
             credentials_dict[line.split()[1]] = [next(f).split()[0],next(f).split()[0]]
 
 
-# In[5]:
+# Get Flatex Account Information
+@st.cache
+def get_flatex_balance():
+
+    # define the url to be scraped
+    URL = "https://www.flatex.at/login/"
+    # start the session
+    driver = webdriver.Safari()
+    driver.get(URL)
+    driver.maximize_window()
+
+    for key in credentials_dict.keys():
+        if key.lower() in URL:
+            user, pw = credentials_dict[key]
+
+    # accept cookies
+    driver.find_element(By.CLASS_NAME,"sg-cookie-optin-box-button-accept-all").click()
+
+    # enter credentials
+    driver.find_element(By.ID,'uname_app').send_keys(user)
+    driver.find_element(By.ID,"password_app").send_keys(pw)
+    # click the login button
+    driver.find_element(By.CSS_SELECTOR,"button.button").click()
+
+    # close current tab
+    driver.close()
+
+    # get current window (the new tab)
+    window = driver.window_handles
+
+    # switch to the new window
+    driver.switch_to.window(window[0])
+
+    # wait for full load
+    wait_for_full_load(driver, "//td[text()='KONTO & DEPOT']")
+
+    # expand overview of the account
+    driver.find_element(By.XPATH, "//td[text()='KONTO & DEPOT']").click()
+
+    # click on depotbestand to get to full overview
+    driver.find_element(By.XPATH, "//div[text()='Depotbestand']").click()
+
+    # wait to load
+    wait_for_full_load(driver, "//td[@class='C4 footer']")
+
+    # extract information on account balance
+    total_flatex_value = driver.find_elements(By.XPATH, "//td[@class='C4 footer']")[0].text.split()[0]
+    total_flatex_paid = driver.find_elements(By.XPATH, "//td[@class='C4 footer']")[1].text.split()[0]
+    absolute_profit = driver.find_elements(By.XPATH, "//td[@class='C5 footer']")[0].text.split()[0]
+    relative_profit = driver.find_elements(By.XPATH, "//td[@class='C5 footer']")[1].text.split()[0]
+    driver.close()
+    print('total flatex value: ',total_flatex_value)
+    print('total flatex paid: ', total_flatex_paid)
+    return (total_flatex_value,absolute_profit)
 
 
-# define the url to be scraped
-URL = "https://www.flatex.at/login/"
-# start the session
-driver = webdriver.Safari()
-driver.maximize_window()
-driver.get(URL)
-# accept cookies
-driver.find_element(By.CLASS_NAME,"sg-cookie-optin-box-button-accept-all").click()
+# Get Raiffeisen Account Information
+@st.cache
+def get_raiffaisen_balance():
 
-for key in credentials_dict.keys():
-    if key.lower() in URL:
-        user, pw = credentials_dict[key]
-# wait until the site is fully loaded
-time.sleep(1)
-# store current window
-current_window = driver.window_handles[0]
+    # define the url to be scraped
+    URL = "https://sso.raiffeisen.at/mein-login/identify"
+    # start the session
+    driver = webdriver.Safari()
+    driver.get(URL)
+    driver.maximize_window()
+    # wait for loading the page
+    wait_for_full_load(driver, "//*[@id='rds-select-0']")
 
-# enter credentials
-driver.find_element_by_id('uname_app').send_keys(user)
-driver.find_element_by_id("password_app").send_keys(pw)
-# click the login button
-driver.find_element(By.CSS_SELECTOR,"button.button").click()
-
-# wait until webpage has loaded
-time.sleep(3)
-
-#get first child window
-windows = driver.window_handles
-
-# extract what the new window is
-windows.remove(current_window)
-new_window = windows[0]
-
-# switch to the new window
-driver.switch_to.window(new_window)
+    # click on the drop down menu
+    driver.find_element(By.XPATH,"//*[@id='rds-select-0']").click()
+    # to select the right bank (Oberösterreich)
+    driver.find_element(By.XPATH,"//*[@id='rds-option-4']/span").click()
 
 
-# expand overview of the account
-driver.find_element(By.XPATH, "//td[text()='KONTO & DEPOT']").click()
+    # extract login information from dict
+    for key in credentials_dict.keys():
+        if key.lower() in URL:
+            user, pw = credentials_dict[key]
 
-# click on depotbestand to get to full overview
-driver.find_element(By.XPATH, "//div[text()='Depotbestand']").click()
+    # input username
+    driver.find_element(By.XPATH,"//*[@id='rds-input-0']").send_keys(user)
 
-# wait to load
-time.sleep(1)
+    # input password
+    driver.find_element(By.XPATH,"//*[@id='rds-input-1']").send_keys(pw)
 
-# extract information on account balance
-total_flatex_value = driver.find_elements(By.XPATH, "//td[@class='C4 footer']")[0].text.split()[0]
-total_flatex_paid = driver.find_elements(By.XPATH, "//td[@class='C4 footer']")[1].text.split()[0]
-absolute_profit = driver.find_elements(By.XPATH, "//td[@class='C5 footer']")[0].text.split()[0]
-relative_profit = driver.find_elements(By.XPATH, "//td[@class='C5 footer']")[1].text.split()[0]
+    # click on submit button to log in
+    driver.find_element(By.XPATH,"//*[@id='rds-grid-row-5-column-5']/button").click()
 
-driver.close()
+    # wait until the site has loaded
+    wait_for_full_load(driver, """//*[@id="rds-scrub-item-1-0"]/div/rds-card/div/rds-card-content/span[2]""")
 
-#get first child window
-chwd = driver.window_handles
+    # extract information on account balance
+    total_raiffeisen_giro_value = driver.find_elements(By.XPATH, """//*[@id="rds-scrub-item-1-0"]/div/rds-card/div/rds-card-content/span[2]""")[0].text.split()[0]
+    print('total raiffaisen giro balance: ',total_raiffeisen_giro_value)
 
-driver.switch_to.window(chwd[0])
+    # extract creditcard balance
+    total_raiffeisen_creditcard_value = driver.find_elements(By.XPATH, """//*[@id="rds-scrub-item-1-1"]/div/rds-card/div/rds-card-content/span[2]""")[0].text.split()[0]
+    print('total raiffaisen creditcard balance: ',total_raiffeisen_creditcard_value)
 
-driver.close()
+    # close driver
+    driver.close()
 
-st.metric(label="Flatex Total Value", value=total_flatex_value + " €", delta=absolute_profit + " €")
+    return (total_raiffeisen_giro_value,total_raiffeisen_creditcard_value)
 
+
+# call functions
+total_flatex_value,absolute_profit = get_flatex_balance()
+total_raiffeisen_giro_value,total_raiffeisen_creditcard_value = get_raiffaisen_balance()
+
+# Display information with streamlit
+
+# create page title
+st.title('My first streamlit application')
+
+
+# display values via metrics
+col1, col2, col3 = st.columns(3)
+col1.metric(label="Flatex Total Value", value=total_flatex_value + " €", delta=absolute_profit + " €")
+col2.metric(label='Raiffeisen Giro Total Value', value = total_raiffeisen_giro_value + " €")
+col3.metric(label='Raiffeisen Creditcard Total Value', value = total_raiffeisen_creditcard_value + " €")
