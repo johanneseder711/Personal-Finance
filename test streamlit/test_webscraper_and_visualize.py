3f29676a561fe4c1328f758834299d72ea25a7b0
Johannes Eder <johanneseder711@MacBook-Pro-von-Johannes.local>
2022-01-03 01:59:32 +0100

    added first charts and visualizations based on csv data

--- test streamlit/test_webscraper_and_visualize.py
+++ test streamlit/test_webscraper_and_visualize.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import pandas as pd
 import re
 import time
-
 import streamlit as st
-
+from datetime import datetime
 
 # Functions
 
@@ -26,6 +26,7 @@ def wait_for_full_load(driver, xpath):
 
 # define the path to the credentials file
 PATH_CREDENTIALS = '../Credentials/my_credentials.txt'
+PATH_DATA = '../data/MoneyControl-CSVExport_2021-12-29.csv'
 
 # read in the file line by line and store the credentials
 # new credentials start with a # and the name of the credentials site
@@ -152,9 +153,27 @@ total_raiffeisen_giro_value,total_raiffeisen_creditcard_value = get_raiffaisen_b
 # create page title
 st.title('My first streamlit application')
 
-
 # display values via metrics
 col1, col2, col3 = st.columns(3)
 col1.metric(label="Flatex Total Value", value=total_flatex_value + " €", delta=absolute_profit + " €")
 col2.metric(label='Raiffeisen Giro Total Value', value = total_raiffeisen_giro_value + " €")
 col3.metric(label='Raiffeisen Creditcard Total Value', value = total_raiffeisen_creditcard_value + " €")
+
+df = pd.read_csv(PATH_DATA,sep=";",parse_dates=['Datum'])
+df['Betrag'] = df.Betrag.str.replace('.','').str.replace(',','.').astype('float')
+df['Tag'] = df.Datum.dt.day
+df['Monat'] = df.Datum.dt.month
+df['Jahr'] = df.Datum.dt.year
+df = df[df.Jahr == 2021]
+
+option = st.selectbox(
+     'Wonach soll gefiltert werden?',
+     ('Datum', 'Kategorie'))
+
+if option == 'Datum':
+    num_elems = st.slider('Wie viele Zeilen sollen betrachtet werden', 0, len(df), 50)
+    st.dataframe(df.sort_values('Datum',ascending=False).iloc[:num_elems])
+    #df_by_day = df.groupby(['Monat']).sum()
+else:
+    df_by_category = df.groupby('Kategorie').sum()
+    st.bar_chart(df_by_category.sort_values('Betrag',ascending=False)['Betrag'])
